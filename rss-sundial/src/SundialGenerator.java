3e7343f608ea4ac5f43049bd27c430bf3fd98ea0
Reid Satsuma <rsatsuma@hawaii.edu>
2013-04-17 00:38:59 -1000

    Locked window size, added hyperlink to website, implemented today's date functionality, cleaned up latitude and longitude textfields and handling.

--- rss-sundial/src/SundialGenerator.java
+++ rss-sundial/src/SundialGenerator.java
@@ -6,12 +6,13 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.util.*;
+import java.net.*;
 
 public class SundialGenerator {
 
 	private JFrame frame;
-	private JTextField txtEnterLongitudeHere;
 	private JTextField txtEnterLatitudeHere;
+	private JTextField txtEnterLongitudeHere;
 
 	/**
 	 * Launch the application.
@@ -41,7 +42,8 @@ public class SundialGenerator {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 548, 350);
+		frame.setResizable(false);
+		frame.setBounds(100, 100, 546, 362);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		JLabel lblSundialGenerator = new JLabel("Sundial Generator");
@@ -54,7 +56,9 @@ public class SundialGenerator {
 		panel.setLayout(new FormLayout(new ColumnSpec[] {
 				ColumnSpec.decode("434px:grow"),},
 			new RowSpec[] {
-				RowSpec.decode("14px"),
+				RowSpec.decode("25px"),
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
 				FormFactory.RELATED_GAP_ROWSPEC,
 				FormFactory.DEFAULT_ROWSPEC,
 				FormFactory.RELATED_GAP_ROWSPEC,
@@ -75,15 +79,18 @@ public class SundialGenerator {
 				FormFactory.DEFAULT_ROWSPEC,}));
 		
 		final JLabel lblStatusReady = new JLabel("Status: Ready");
+		lblStatusReady.setBackground(Color.WHITE);
+		lblStatusReady.setForeground(Color.RED);
+		lblStatusReady.setFont(new Font("Tahoma", Font.BOLD, 14));
 		lblStatusReady.setHorizontalAlignment(SwingConstants.CENTER);
-		panel.add(lblStatusReady, "1, 1, fill, top");
+		panel.add(lblStatusReady, "1, 1, fill, center");
 		
 		JLabel lblDate = new JLabel("Date");
 		lblDate.setFont(new Font("Tahoma", Font.BOLD, 11));
 		panel.add(lblDate, "1, 3, center, default");
 		
 		JPanel panel_1 = new JPanel();
-		panel.add(panel_1, "1, 5, fill, center");
+		panel.add(panel_1, "1, 7, fill, center");
 		panel_1.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
 		
 		final JComboBox<String> comboBox = new JComboBox<String>();
@@ -96,41 +103,102 @@ public class SundialGenerator {
 		comboBox_1.setMaximumRowCount(5);
 		panel_1.add(comboBox_1);
 		
-		JLabel lblLongitude = new JLabel("Longitude");
-		lblLongitude.setFont(new Font("Tahoma", Font.BOLD, 11));
-		panel.add(lblLongitude, "1, 7, center, default");
-		
-		txtEnterLongitudeHere = new JTextField();
-		txtEnterLongitudeHere.setText("Enter Longitude Here");
-		panel.add(txtEnterLongitudeHere, "1, 9, fill, default");
-		txtEnterLongitudeHere.setColumns(10);
+		final JCheckBox chckbxUseTodaysDate = new JCheckBox("Use Today's Date");
+		chckbxUseTodaysDate.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (chckbxUseTodaysDate.isSelected())
+				{
+					//lblStatusReady.setText("Status: Using today's date!");
+					comboBox.setEnabled(false);
+					comboBox_1.setEnabled(false);
+				}
+				else if (!chckbxUseTodaysDate.isSelected())
+				{
+					//lblStatusReady.setText("Status: Not using today's date!");
+					comboBox.setEnabled(true);
+					comboBox_1.setEnabled(true);
+				}
+			}
+		});
+		panel.add(chckbxUseTodaysDate, "1, 5, center, default");
 		
 		JLabel lblLatitude = new JLabel("Latitude");
 		lblLatitude.setFont(new Font("Tahoma", Font.BOLD, 11));
-		panel.add(lblLatitude, "1, 11, center, default");
+		panel.add(lblLatitude, "1, 9, center, default");
 		
 		txtEnterLatitudeHere = new JTextField();
 		txtEnterLatitudeHere.setText("Enter Latitude Here");
-		panel.add(txtEnterLatitudeHere, "1, 13, fill, default");
-		txtEnterLatitudeHere.setColumns(10);
+		panel.add(txtEnterLatitudeHere, "1, 11, center, center");
+		txtEnterLatitudeHere.setColumns(40);
+		
+		JLabel lblLongitude = new JLabel("Longitude");
+		lblLongitude.setFont(new Font("Tahoma", Font.BOLD, 11));
+		panel.add(lblLongitude, "1, 13, center, default");
+		
+		txtEnterLongitudeHere = new JTextField();
+		txtEnterLongitudeHere.setText("Enter Longitude Here");
+		panel.add(txtEnterLongitudeHere, "1, 15, center, default");
+		txtEnterLongitudeHere.setColumns(40);
 		
 		JLabel lblMesaVerdeReseach = new JLabel("Mesa Verde Research Group");
-		panel.add(lblMesaVerdeReseach, "1, 19, center, default");
+		lblMesaVerdeReseach.setForeground(Color.BLUE);
+		lblMesaVerdeReseach.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				
+				try
+				{
+					URI url = new URI("https://code.google.com/p/rss-sundial/");
+					
+					if (Desktop.isDesktopSupported())
+		            {
+		                try
+		                {
+		                    Desktop.getDesktop().browse(url);
+		                }
+		                catch (Exception e)
+		                {
+		                    // CANNOT BROWSE, DO NOTHING FOR NOW
+		                }
+		            }
+		            else
+		            {
+		                // DESKTOP IS NOT SUPPORTED, DO NOTHING FOR NOW
+		            }
+				}
+				catch (URISyntaxException e)
+				{
+					// URISyntaxException, do nothing for now
+				}	
+			}
+		});
+		panel.add(lblMesaVerdeReseach, "1, 21, center, default");
 		
 		JButton btnGenerateSundial = new JButton("Generate Sundial");
 		btnGenerateSundial.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-								
-				// Initializing month and day variables
-				int month = comboBox.getSelectedIndex() + 1;
+				
+				Calendar cal = Calendar.getInstance();
+				int year = 0;
+				String yearString = Integer.toString(year);
+				year = cal.get(Calendar.YEAR);
+				int month = 0;
 				String monthString = "";
-				int day = comboBox_1.getSelectedIndex() + 1;
+				int day = 0;
 				String dayString = "";
 				
-				// Getting the current year
-				Calendar cal = Calendar.getInstance();
-				int year = cal.get(Calendar.YEAR);
-				String yearString = Integer.toString(year);
+				if (chckbxUseTodaysDate.isSelected())
+				{
+					day = cal.get(Calendar.DATE);
+					month = cal.get(Calendar.MONTH);
+				}
+				else
+				{
+					// Initializing month and day variables
+					month = comboBox.getSelectedIndex() + 1;
+					day = comboBox_1.getSelectedIndex() + 1;
+				}
+				
 				
 				// Assuring month and day are in 2 digit format
 				if (month < 10)
@@ -157,21 +225,21 @@ public class SundialGenerator {
 				try
 				{
 					// Initializing longitude and latitude variables from inputs
-					double longitude = Double.parseDouble(txtEnterLongitudeHere.getText());
 					double latitude = Double.parseDouble(txtEnterLatitudeHere.getText());
+					double longitude = Double.parseDouble(txtEnterLongitudeHere.getText());
 					
 					// Longitude and Latitude input validity checking
 					if (Math.abs(longitude) > 180 && Math.abs(latitude) > 90)
 					{
-						lblStatusReady.setText("Status: Invalid longitude and latitude!");
-					}
-					else if (Math.abs(longitude) > 180)
-					{
-						lblStatusReady.setText("Status: Invalid longitude!");
+						lblStatusReady.setText("Status: Invalid latitude and longitude!");
 					}
 					else if (Math.abs(latitude) > 90)
 					{
 						lblStatusReady.setText("Status: Invalid latitude!");
+					}
+					else if (Math.abs(longitude) > 180)
+					{
+						lblStatusReady.setText("Status: Invalid longitude!");
 					}
 					// Recieved valid inputs
 					else
@@ -191,7 +259,7 @@ public class SundialGenerator {
 				// Catches longitude and latitude inputs that cannot be parsed into numbers
 				catch (NumberFormatException e)
 				{
-					lblStatusReady.setText("Status: Please enter numerical digits as longitude and latitude values!");
+					lblStatusReady.setText("Status: Please enter longitude and latitude as numerical values!");
 				}
 				// Catches interrupt exception for the Thread.sleep() command
 				catch (InterruptedException e)
